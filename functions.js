24bc8b483fe49dc51713aae82f6e619c57901697
Johny Junior Duval <johny.duval@gmail.com>
2015-11-16 22:13:56 +0000

    comparing names and emails of arrays of objects

--- functions.js
+++ functions.js
@@ -12,3 +12,49 @@ function compare (string1, string2){
 var numbers = ["banana", "oat"];
 
 numbers.sort(compare);
+
+// Sort by name and then by alphabetical order of email
+
+//creating the array of objects
+
+var arrayOfObjects = [
+    {
+        name: "hello" ,
+        email: 'hello@gmail.com'
+    },
+    {
+        name: "info",
+        email: 'info@gmail.com'
+    }
+
+];
+
+// comparing names
+
+function compareNames (string1, string2){
+    
+    if (string1.length > string2.length){
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+var objectNames = [arrayOfObjects[0].name, arrayOfObjects[1].name];
+
+objectNames.sort(compareNames);
+
+// comparing emails
+
+function compareEmails (string1, string2){
+    
+    if (string1 > string2){
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+var objectEmails = [arrayOfObjects[0].email, arrayOfObjects[1].email];
+
+objectEmails.sort(compareEmails);
